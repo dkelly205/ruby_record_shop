f536db50133a0c10195bdb715e7c0867a2d89a12
daniel kelly <danieljkelly91@gmail.com>
2018-05-17 20:10:31 +0100

    adds test for stock level in specs/album_spec

--- specs/album_spec.rb
+++ specs/album_spec.rb
@@ -11,30 +11,34 @@ class TestAlbum < Minitest::Test
       'name' => 'Oasis',
       'image' => 'oasis.jpeg'})
 
-    @album = Album.new({
+      @album = Album.new({
         'id' => 1,
         'title' => 'Parachutes',
         'quantity' => '10',
         'artist_id' => @artist.id})
-  end
+      end
 
-  def test_album_id
-    assert_equal( 1, @album.id() )
-  end
+      def test_album_id
+        assert_equal( 1, @album.id() )
+      end
 
-  def test_album_title
-    assert_equal( "Parachutes", @album.title() )
-  end
+      def test_album_title
+        assert_equal( "Parachutes", @album.title() )
+      end
 
-  def test_album_has_quantity
-    assert_equal( 10, @album.quantity() )
-  end
+      def test_album_has_quantity
+        assert_equal( 10, @album.quantity() )
+      end
 
 
-    def test_album_has_artist_id
-      assert_equal(3, @album.artist_id() )
+      def test_album_has_artist_id
+        assert_equal(3, @album.artist_id() )
+      end
+
+      def test_album_stock_level
+        assert_equal("Low", @album.stock_count)
+      end
+
+
+
     end
-
-
-
-end
